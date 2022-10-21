88a9a6bbb43c9ae765e3000b32d958e6b2536081
hyeonpark-5 <67797025+hyeonpark-5@users.noreply.github.com>
2022-10-21 20:43:05 +0900

    Add files via upload

---
+++ CodeUP_basic_study/CodeUp_10_20.py
@@ -0,0 +1,43 @@
+#6011 #11
+#n = float(input())
+#print(n)
+
+#6012 #12
+#a = int(input())
+#b = int(input())
+#print(a)
+#print(b)
+
+#6013 #13
+#a = input()
+#b = input()
+#print(b)
+#print(a)
+
+#6014 #14
+#a = float(input())
+#print(f"{a}\n{a}\n{a}")
+
+#6015 #15
+#a, b = map(int, input().split())
+#print(f"{a}\n{b}")
+
+#6016 #16
+#a, b = map(str, input().split())
+#print(b,a)
+
+#6017 #17
+#a = input()
+#print(f"{a} "*3 )
+
+#6018 #18
+#hour, minute = input().split(':')
+#print(hour,minute,sep=':')
+
+#6019 #19
+#y, m, d = input().split('.')
+#print(d,m,y,sep='-')
+
+#6020 #20
+#a, b =input().split('-')
+#print(a+b)
