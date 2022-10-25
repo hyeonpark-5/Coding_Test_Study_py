95622610d0f8a1474a4e779747feb6727ecac9b6
hyeonpark-5 <67797025+hyeonpark-5@users.noreply.github.com>
2022-10-25 23:27:24 +0900

    Add files via upload

---
+++ CodeUP_basic_study/CodeUp_31_40.py
@@ -0,0 +1,40 @@
+#6031 #31
+# a = int(input())
+# print(chr(a))
+
+#6032 #32
+# a = int(input())
+# print(-a)
+
+#6033 #33
+# a = ord(input())
+# print(chr(a+1))
+
+#6034 #34
+# a, b = map(int, input().split())
+# print(a - b)
+
+#6035 #35
+# a, b = map(float, input().split())
+# print(a * b)
+
+#6036 #36
+# a, b = input().split()
+# print(a * int(b))
+
+#6037 #37
+# a = int(input())
+# b = input()
+# print(a *b, sep= ' ')
+
+#6038 #38
+# a, b = map(int, input().split())
+# print(a**b)
+
+#6039 #39
+# f1, f2 = map(float, input().split())
+# print(f1**f2)
+
+#6040 #40
+# f1, f2 = map(int, input().split())
+# print(f1//f2)
