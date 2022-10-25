286aa46fac795d353e9d462810dded45c03dc039
hyeonpark-5 <67797025+hyeonpark-5@users.noreply.github.com>
2022-10-25 23:27:08 +0900

    Add files via upload

---
+++ CodeUP_basic_study/CodeUp_21_30.py
@@ -0,0 +1,51 @@
+#6021 #21
+# S = input()
+
+# for i in range(len(S)):
+#     print(S[i])
+
+#6022 #22
+# S = input()
+
+# print(S[0:2], end=" ")
+# print(S[2:4], end=" ")
+# print(S[4:6], end=" ")
+
+#6023 #23
+# S = input()
+# m = S.split(':')
+
+# print(m[1])
+
+#6024 #24
+# S = input()
+# M= S.split()
+# print(f"{M[0]}{M[1]}")
+
+#6025 #25
+# a, b = input().split()
+# sum = int(a)+int(b)
+# print(sum)
+
+#6026 #26
+# a = input()
+# b = input()
+# sum = float(a)+float(b)
+# print(sum)
+
+#6027 #27
+# a = int(input())
+# print("%x"%a)
+
+#6028 #28
+# a = int(input())
+# print("%X"%a)
+
+#6029 #29
+# a = input()
+# n = int(a,16)
+# print("%o"%n)
+
+#6030 #30
+# a = ord(input())
+# print(a)
