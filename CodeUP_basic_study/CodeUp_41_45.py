18d0da68f6a41a4e6159e73bcceaa116f336e6d5
hyeonpark-5 <67797025+hyeonpark-5@users.noreply.github.com>
2022-10-26 23:03:46 +0900

    Add files via upload

---
+++ CodeUP_basic_study/CodeUp_41_45.py
@@ -0,0 +1,31 @@
+#6041 #41
+# f1, f2 = map(int, input().split())
+# print(f1%f2)
+
+#6042 #42
+# a = float(input())
+# print("%.2f"%a)
+
+#6043 #43
+# f1 , f2 = map(float, input().split())
+# a = f1 / f2
+# print("%.3f"%a)
+
+
+#6044 #44
+# f1, f2 = map(int, input().split())
+# a = f1/f2
+# print(f1+f2)
+# print(f1-f2)
+# print(f1*f2)
+# print(f1//f2)
+# print(f1%f2)
+
+# print("%.2f"%a)
+
+#6045 #45
+# f1, f2, f3 = map(int, input().split())
+# sum = f1 + f2 + f3
+# p = round(sum / 3,2)
+# print(sum ,end=" ")
+# print("%.2f"%p)
