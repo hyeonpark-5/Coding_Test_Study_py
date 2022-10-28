21e0de4d5590357d89b09b6131f3a538ec49f888
hyeonpark-5 <67797025+hyeonpark-5@users.noreply.github.com>
2022-10-28 19:52:52 +0900

    Add files via upload

---
+++ CodeUP_basic_study/CodeUp_46_58.py
@@ -0,0 +1,53 @@
+#6046 #46
+# a = int(input())
+# print(a<<1)
+    
+#6047 #47
+# a , b = map(int, input().split())
+# print(a<<b)
+    
+#6048 #48
+# a , b = map(int, input().split())
+# print(a<b)
+
+#6049 #49
+# a , b = map(int, input().split())
+# print(a==b)
+
+#6050
+# a , b = map(int, input().split())
+# print(b >= a)
+
+#6051 #51
+# a , b = map(int, input().split())
+# print(a != b)
+
+#6052 #52
+# a = int(input())
+# print(a != 0)
+
+#6053 #53
+# a = int(input())
+# b = bool(a)
+# print(b != True)
+ 
+ #6054 #54
+#  a,b = map(int, input().split())
+#  print(bool(a) and bool(b))
+
+#6055 #55
+# a,b = map(int, input().split())
+# print(bool(a) or bool(b))
+
+#6056 #56
+# a,b = map(int, input().split())
+# print((not(bool(a)) and bool(b)) or (bool(a) and not(bool(b))))
+
+ #6057 #57
+# a,b = map(int, input().split())
+# print((not(bool(a)) and bool(b)) or (bool(a) and not(bool(b))))   
+
+#6058 #58
+# a,b = map(int, input().split())
+# print(not(bool(a)) and not(bool(b)))
+
