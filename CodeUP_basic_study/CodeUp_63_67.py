15bc92f24f1f23e1c9ea5a47c2847a7e700a6fff
hyeonpark-5 <67797025+hyeonpark-5@users.noreply.github.com>
2022-10-29 23:23:06 +0900

    Add files via upload

---
+++ CodeUP_basic_study/CodeUp_63_67.py
@@ -0,0 +1,40 @@
+#6063 #63
+# a,b = map(int, input().split())
+# if a > b:
+#     print(a)
+# else:
+#     print(b)
+
+#6064 #64
+# a,b,c = map(int, input().split())
+# if a < b and a < c:
+#     print(a)
+# elif b < a and b < c:
+#     print(b)
+# else:
+#     print(c)
+
+#6065 #65
+# text= list(map(int, input().split()))
+# for i in text:
+#     if int(i) % 2 == 0:
+#         print(i)
+
+#6066 #66
+# text= list(map(int, input().split()))
+# for i in text:
+#     if int(i) % 2 == 0:
+#         print('even')
+#     else:
+#         print('odd')
+
+#6067 #67
+# a = int(input())
+# if a % 2 == 0 and a < 0:
+#     print('A')
+# elif a % 2 == 1 and a < 0:
+#     print('B')
+# elif a % 2 == 0 and a > 0:
+#     print('C')
+# else:
+#     print('D')
