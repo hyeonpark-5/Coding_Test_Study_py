f11ea5217b6af8a23df2435706e24cc7b427f8b3
hyeonpark-5 <67797025+hyeonpark-5@users.noreply.github.com>
2022-10-30 20:43:51 +0900

    Add files via upload

---
+++ CodeUP_basic_study/CodeUp_68_77.py
@@ -0,0 +1,87 @@
+#6068 #68
+# score = int(input())
+
+# if score >= 90:
+#     print('A')
+# elif score >= 70:
+#     print('B')
+# elif score >= 40:
+#     print('C')
+# else:
+#     print('D')
+
+#6069 #69
+# a = input()
+
+# if a == 'A':
+#     print('best!!!')
+# elif a == 'B':
+#     print('good!!')
+# elif a == 'C':
+#     print('run!')
+# elif a == 'D':
+#     print('slowly~')
+# else:
+#     print('what?')
+
+#6070 #70
+# a = int(input())
+
+# if a == 12 or 1 <= a <=2:
+#     print('winter')
+# elif 3 <= a <= 5:
+#     print('spring')
+# elif 6 <= a <= 8:
+#     print('summer')
+# elif 9 <= a <= 11:
+#     print('fall')
+
+#6071 #71
+# while True:
+#     a = int(input())
+#     if a == 0:
+#         break
+#     else:
+#         print(a)
+
+#6072 #72
+# a = int(input())
+
+# while a > 0:
+#     print(a)
+#     a -=1
+
+#6073 #73
+# a = int(input())
+# while True:
+#     if a == 0:
+#         break
+#     else:
+#         a -= 1
+#         print(a)
+
+#6074 #74
+# a = ord(input())
+# b = ord('a')
+# while b <= a:
+#     print(chr(b), end=' ')
+#     b += 1
+
+#6075 #75
+# a = int(input())
+# for i in range(a+1,):
+#     print(i)
+
+#6076 #76
+# a = int(input())
+# for i in range(a+1,):
+#     print(i)
+
+#6077 #77
+# a = int(input())
+# sum = 0
+# for i in range(1,a+1):
+#     if i % 2 == 0:
+#         sum += i
+
+# print(sum)
